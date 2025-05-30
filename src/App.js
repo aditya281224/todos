6e7ba56765401ecdddce55ac178765cd204914a9
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-05-30 23:40:45 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todo, setTodoList] = useState([
+  ]);
+
+  const handlechange = (input) => {
+    const arr = [...todo];
+    arr.push({
+      id: todo.length + 1,
+      text: input,
+      completed: false,
+    });
+    setTodoList(arr);
+    setInput("");
+    console.log(arr);
+  };
+
+  const delteHandler = (index) => {
+    const newArr = [...todo];
+    newArr.splice(index, 1);
+    setTodoList(newArr);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+
+      <div className="input-section">
+        <input
+          type="text"
+          placeholder="Enter todo"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button onClick={() => handlechange(input)}>Add</button>
+      </div>
+
+      <ul>
+        {todo.map((t, index) => (
+          <li key={t.id}>
+            <input
+              type="checkbox"
+              checked={t.completed}
+              onChange={() => {
+                const updatedTodos = [...todo];
+                updatedTodos[index].completed = !updatedTodos[index].completed;
+                setTodoList(updatedTodos);
+              }}
+            />
+
+            <span className={t.completed ? "stripe" : ""}>{t.text}</span>
+            <button onClick={() => delteHandler(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
